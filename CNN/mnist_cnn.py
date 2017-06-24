52520af65f9400eade0a82ad94ba59915d145b0f
a-simada <a-simada@ics.es.osaka-u.ac.jp>
2017-06-24 23:53:49 +0900

    fix

--- CNN/mnist_cnn.py
+++ CNN/mnist_cnn.py
@@ -105,7 +105,7 @@ if __name__ == '__main__':
         # 精度
         acc_sum = 0
         # 学習時のバッチのシャッフル(only np)
-        perm = np.random.permutation(train_size)
+        perm = xp.asarray(np.random.permutation(train_size))
 
         # バッチ単位での学習
         for i in range(0, train_size, batch_size):
