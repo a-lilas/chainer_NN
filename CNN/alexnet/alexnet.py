3463566b66844f9953922ddd56636ba28e4b23d8
a-simada <a-simada@ics.es.osaka-u.ac.jp>
2017-06-26 14:45:16 +0900

    mkdir alexnet

---
+++ CNN/alexnet/alexnet.py
@@ -0,0 +1,225 @@
+# coding:utf-8
+'''
+AlexNetを用いたMNIST手書き文字識別
+'''
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+import _pickle
+import time
+import chainer
+from chainer import cuda
+from chainer import Function, Variable, optimizers
+from chainer import Link, Chain, ChainList
+import chainer.functions as F
+import chainer.links as L
+from chainer.datasets import get_cifar10
+import util
+
+
+class Alexnet(Chain):
+    # Like Alexnet for MNIST
+    def __init__(self, input_size):
+        '''
+        AlexNet
+        conv1=L.Convolution2D(None, 96, 11, stride=1),
+        conv2=L.Convolution2D(None, 256, 5, stride=1, pad=1),
+        conv3=L.Convolution2D(None, 384, 3, stride=1, pad=1),
+        conv4=L.Convolution2D(None, 384, 3, stride=1, pad=1),
+        conv5=L.Convolution2D(None, 256, 3, stride=1, pad=1),
+        fc6=L.Linear(None, 4096),
+        fc7=L.Linear(None, 4096),
+        fc8=L.Linear(None, 1000)
+        '''
+        super(Alexnet, self).__init__(
+            # None: 出力ノード数の自動推定
+            # Like AlexNet
+            conv1=L.Convolution2D(None, 64, 5, stride=1),
+            conv2=L.Convolution2D(None, 128, 5, stride=1, pad=1),
+            conv3=L.Convolution2D(None, 192, 3, stride=1, pad=1),
+            conv4=L.Convolution2D(None, 192, 3, stride=1, pad=1),
+            conv5=L.Convolution2D(None, 128, 3, stride=1, pad=1),
+            fc6=L.Linear(None, 1024),
+            fc7=L.Linear(None, 512),
+            fc8=L.Linear(None, 10)
+            )
+
+    def __call__(self, x, train=True):
+        # x: 入力， train: 学習時はTrueに．テスト時はFalseにする
+        # LCN: 局所コンストラスト正規化
+        # ある1つのチャンネルについて画素全体で正規化する
+        # LRN: ? (Alexnetはこっち)
+        # ある画素(局所領域)について，多チャンネル全体で正規化する
+        # 本の"norm"と"pool"の順番ってこれでいいの？逆では？
+
+        ######################################
+        h = self.conv1(x)
+        h = F.local_response_normalization(h)
+        h = F.relu(h)
+        h = F.max_pooling_2d(h, 3, stride=2)
+        ######################################
+        h = self.conv2(h)
+        h = F.local_response_normalization(h)
+        h = F.relu(h)
+        h = F.max_pooling_2d(h, 3, stride=2)
+        ######################################
+        h = self.conv3(h)
+        h = F.relu(h)
+        ######################################
+        h = self.conv4(h)
+        h = F.relu(h)
+        ######################################
+        h = self.conv5(h)
+        h = F.relu(h)
+        h = F.max_pooling_2d(h, 3, stride=2)
+        ######################################
+        h = self.fc6(h)
+        h = F.relu(h)
+        h = F.dropout(h)
+        ######################################
+        h = self.fc7(h)
+        h = F.relu(h)
+        h = F.dropout(h)
+        ######################################
+        y = self.fc8(h)
+        ######################################
+
+        return y
+
+
+if __name__ == '__main__':
+    # GPUフラグ
+    gpu_fg = util.gpuCheck(sys.argv)
+    if gpu_fg >= 0:
+        cuda.check_cuda_available()
+    xp = cuda.cupy if gpu_fg >= 0 else np
+
+    # x_train: 32*32*3
+    train, test = get_cifar10()
+    x_train, t_train = train._datasets
+    x_test, t_test = test._datasets
+
+    # 学習データサイズ
+    input_size = 32
+    # 学習データ数
+    train_size = len(x_train)
+    # テストデータ数
+    test_size = len(x_test)
+    # エポック数
+    epoch_n = 100
+    # バッチサイズ
+    batch_size = 64
+    # for plot
+    loss_list = []
+    acc_sum_test_list = []
+    acc_sum_train_list = []
+
+    # 2次元配列を4次元配列に変換(枚数とチャンネル数を追加)
+    x_train = np.asarray(np.reshape(x_train, (train_size, 3, 32, 32)))
+    x_test = np.asarray(np.reshape(x_test, (test_size, 3, 32, 32)))
+    t_train = np.asarray(t_train)
+    t_test = np.asarray(t_test)
+
+    # model
+    model = Alexnet(input_size=input_size)
+    # to GPU
+    if gpu_fg >= 0:
+        cuda.get_device(gpu_fg).use()
+        model.to_gpu()
+
+    # optimizer
+    optimizer = optimizers.Adam()
+    optimizer.setup(model)
+
+    # Training Loop
+    for epoch in range(0, epoch_n):
+        # time per epoch
+        start_time = time.time()
+
+        # 誤差 初期値
+        loss_sum = 0
+        # 精度
+        acc_sum_train = 0
+        acc_sum_test = 0
+        # 学習時のバッチのシャッフル(only np)
+        perm = np.asarray(np.random.permutation(train_size))
+
+        # バッチ単位での学習
+        for i in range(0, train_size, batch_size):
+            # x: データ, t: ラベル
+            # バッチ作成
+            x_batch = xp.asarray(x_train[perm[i:(i+batch_size) if (i+batch_size) < train_size else train_size]])
+            t_batch = xp.asarray(t_train[perm[i:(i+batch_size) if (i+batch_size) < train_size else train_size]])
+            x_batch = x_batch.astype(xp.float32)
+            # インデックスが要素数をオーバーした場合の処理
+            x = Variable(x_batch)
+            t = Variable(t_batch)
+            # 勾配のゼロ初期化
+            model.zerograds()
+            # y: 予測(学習)
+            y = model(x)
+            # 損失関数(ソフトマックス->交差エントロピー)
+            loss = F.softmax_cross_entropy(y, t)
+            # 誤差逆伝搬
+            loss.backward()
+            loss.unchain_backward()
+            # 損失関数を計算
+            # 出力時は，".data"を参照
+            loss_sum += loss.data * len(y)
+            # 最適化
+            optimizer.update()
+            # 識別率を計算
+            acc = F.accuracy(y, t)
+            acc_sum_train += float(acc.data) * len(y)
+
+        # バッチ単位でのテスト
+        for i in range(0, test_size, batch_size):
+            # x: データ, t: ラベル
+            x_batch = xp.asarray(x_test[i:(i+batch_size) if (i+batch_size) < test_size else test_size])
+            t_batch = xp.asarray(t_test[i:(i+batch_size) if (i+batch_size) < test_size else test_size])
+            x_batch = x_batch.astype(xp.float32)
+            x = Variable(x_batch)
+            t = Variable(t_batch)
+
+            # test mode
+            with chainer.using_config('train', False):
+                y = model(x)
+
+            # 識別率を計算
+            acc = F.accuracy(y, t)
+            acc_sum_test += float(acc.data) * len(y)
+
+        # 訓練誤差, 識別率, 学習時間
+        print('epoch: {}'.format(epoch))
+        print('softmax cross entropy: {}'.format(loss_sum / train_size))
+        print('accuracy(train data): {}'.format(acc_sum_train / train_size))
+        print('accuracy(test data): {}'.format(acc_sum_test / test_size))
+        print('time per epoch: {} [sec]'.format(time.time() - start_time))
+        print(' - - - - - - - - - ')
+
+        # append list to plot
+        loss_list.append(float(loss_sum/train_size))
+        acc_sum_train_list.append(float(acc_sum_train/train_size))
+        acc_sum_test_list.append(float(acc_sum_test/test_size))
+
+    # save the model (dump)
+    model.to_cpu()
+    _pickle.dump(model, open("AlexNet.pkl", "wb"), -1)
+
+    # plot loss/acc
+    x = np.arange(0, epoch_n, 1)
+    plt.plot(x, loss_list)
+    plt.title('Softmax cross entropy')
+    xlabels = [0, 20, 40, 60, 80, 100]
+    plt.xticks(xlabels, xlabels)
+    plt.show()
+    plt.plot(x, acc_sum_train_list, color='red', label='train accuracy')
+    plt.plot(x, acc_sum_test_list, color='blue', label='test accuracy')
+    plt.title('Accuracy')
+    plt.legend(loc='lower right')
+    plt.xticks(xlabels, xlabels)
+    plt.show()
+
+    np.save('AlexNet_loss.npy', loss_list)
+    np.save('AlexNet_acc_train.npy', acc_sum_train_list)
+    np.save('AlexNet_acc_test.npy', acc_sum_test_list)
